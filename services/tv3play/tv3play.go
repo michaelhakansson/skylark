bf0e1de9655d8d2926fe1a5b7b675d764601bdf6
michaelhakansson <michael.hakansson@gmail.com>
2015-04-23 20:24:47 +0200

    [tv3play] add more episode info parsing

--- services/tv3play/tv3play.go
+++ services/tv3play/tv3play.go
@@ -4,11 +4,12 @@ package main
 import(
     "bytes"
     "encoding/json"
-    "encoding/xml"
+    // "encoding/xml"
     "fmt"
     "io/ioutil"
     "net/http"
-    "regexp"
+    // "regexp"
+    "strconv"
     "strings"
     "time"
     "github.com/PuerkitoBio/goquery"
@@ -19,82 +20,80 @@ const(
     playService string = "tv3play"
     playUrlBase string = "http://www.tv3play.se/"
     videoUrlBase string = playUrlBase + "program/"
-    jsonVideoOutputString string = "?output=json&format=json"
+    // Should be followed by the episode id
+    jsonVideoOutputString string = "http://playapi.mtgx.tv/v3/videos/"
     allProgramsPage string = playUrlBase + "program"
     rssUrl string = "/rss.xml"
     // The show URL should be followed by the show id after 'format='
     showJsonUrl string = "http://playapi.mtgx.tv/v1/sections?sections=videos.one,seasons.videolist&format="
     xmlDateLayout string = "Mon, 2 Jan 2006 15:04:05 MST"
+    thumbnailSize string = "200x200"
 )
 
+// struct for show information
 type Show struct {
     PlayId string
     PlayService string
     Title string
 }
 
+// struct for episode information
 type Episode struct {
-    Broadcasted time.Time
-    Category string
-    Description string
-    EpisodeNumber int64
-    Length string
-    Live bool
-    PlayId int64
-    Season int64
-    Thumbnail string
-    Title string
-    VideoUrl string
+    Broadcasted time.Time   // TODO: Maybe changed to "air_at"
+    Category string         // DONE
+    Description string      // DONE
+    EpisodeNumber int64     // DONE
+    Length string           // DONE
+    Live bool               // Can't find
+    PlayId int64            // DONE
+    Season int64            // DONE
+    Thumbnail string        // DONE
+    Title string            // DONE
+    VideoUrl string         // TODO: If not HLS found --> use sharing.url URL
 }
 
+// structs for an episode's json output
 type Program struct {
-    Context Context
-    Statistics Statistics
-    VideoId int64
-    Video Video
+    Id int64 // Id of the video
+    Format_title string // The title of the show (without s. and ep. number)
+    Format_position Format_position
+    Embedded Embedded `json:"_embedded"` // Used to get thumbnail url
+    Format_categories []Format_categories
+    Description string
+    Duration int64
+    Publish_at string // Used as the broadcast date
 }
 
-type Context struct {
-    Title string
-    ProgramTitle string
-    ThumbnailImage string
+//Contains season and episode number
+type Format_position struct {
+    Season int64
+    Episode string
 }
 
-type Statistics struct {
-    BroadcastDate string
-    BroadcastTime string
-    Category string
+// Contains the category of the show
+type Format_categories struct {
+    Name string
 }
 
-type Video struct {
-    AvailableOnMobile bool
-    Live bool
-    MaterialLength int64
-    Position int64
-    VideoReferences []VideoReferences
+type Embedded struct {
+    Format Format
 }
 
-type VideoReferences struct {
-    Bitrate int64
-    PlayerType string
-    Url string
+type Format struct {
+    Links Links `json:"_links"`
 }
 
-type Channel struct {
-    XMLName xml.Name `xml:"rss"`
-    Title string `xml:"channel>title"`
-    Item []Item `xml:"channel>item"`
+type Links struct {
+    Image Image
 }
 
-type Item struct {
-    Title string `xml:"title"`
-    Link string `xml:"link"`
-    Description string `xml:"description"`
-    PubDate string `xml:"pubDate"`
-    Guid int64 `xml:"guid"`
+type Image struct {
+    Href string
 }
 
-/* Fetches all show id's from the provider */
+// GetAllProgramIds fetches from the provider all of the programs id's
+// By parsing the "all program page" of the provider
+// Returns an array of all the id's in the form of a string array
 func GetAllProgramIds() (programs []string) {
     // Get all program links from the program list
     b := getPage(allProgramsPage)
@@ -122,87 +121,101 @@ func GetAllProgramIds() (programs []string) {
         if len(id) > 0 {
             programs = append(programs, id)
         }
-        // fmt.Println(id)
     }
 
     return
 }
 
-/* Fetches show information and episodes for that show */
-func GetShow(showId string) (Show, []Episode) {
-    url := showJsonUrl + showId
-    b := getPage(url)
-    if len(b) > 0 {
-        return parseShowXML(b, showId)
-    } else {
-        pageUrl := playUrlBase + showId
-        return parseShowPage(getPage(pageUrl), showId)
-    }
-}
+// // GetShow fetches the information and all the episodes for a show
+// func GetShow(showId string) (Show, []Episode) {
+//     url := showJsonUrl + showId
+//     b := getPage(url)
+//     if len(b) > 0 {
+//         return parseShowXML(b, showId)
+//     } else {
+//         pageUrl := playUrlBase + showId
+//         return parseShowPage(getPage(pageUrl), showId)
+//     }
+// }
 
-func parseShowXML(page []byte, showId string) (show Show, episodes []Episode) {
-    var c Channel
-    err := xml.Unmarshal(page, &c)
-    checkerr(err)
-    show.Title = strings.Replace(c.Title, " - Senaste program", "", 1)
-    show.PlayService = playService
-    show.PlayId = showId
-    r, err := regexp.Compile(`\/\d+\/`)
-    checkerr(err)
-    for _, item := range c.Item {
-        shortLink := r.FindString(item.Link)
-        episodeId := strings.Replace(string(shortLink), "/", "", 2)
-        e := GetEpisode(episodeId)
-        episodes = append(episodes, e)
-    }
-    return
-}
+// func parseShowXML(page []byte, showId string) (show Show, episodes []Episode) {
+//     var c Channel
+//     err := xml.Unmarshal(page, &c)
+//     checkerr(err)
+//     show.Title = strings.Replace(c.Title, " - Senaste program", "", 1)
+//     show.PlayService = playService
+//     show.PlayId = showId
+//     r, err := regexp.Compile(`\/\d+\/`)
+//     checkerr(err)
+//     for _, item := range c.Item {
+//         shortLink := r.FindString(item.Link)
+//         episodeId := strings.Replace(string(shortLink), "/", "", 2)
+//         e := GetEpisode(episodeId)
+//         episodes = append(episodes, e)
+//     }
+//     return
+// }
 
-func parseShowPage(page []byte, showId string) (show Show, episodes []Episode) {
-    var ids []string
-    reader := bytes.NewReader(page)
-    doc, err := goquery.NewDocumentFromReader(reader)
-    checkerr(err)
-    r, err := regexp.Compile(`\/\d+\/`)
-    checkerr(err)
-    show.Title = doc.Find(".play_title-page-info__header-title").Text()
-    show.PlayService = playService
-    show.PlayId = showId
-    doc.Find(".play_vertical-list").First().Find("li").Each(func(i int, s *goquery.Selection) {
-        link, _ := s.Find(".play_vertical-list__header-link").Attr("href")
-        digi := r.FindString(link)
-        cleanId := strings.Replace(string(digi), "/", "", 2)
-        ids = append(ids, cleanId)
-    })
-    for _, id := range ids {
-        e := GetEpisode(id)
-        episodes = append(episodes, e)
-    }
-    return
-}
+// func parseShowPage(page []byte, showId string) (show Show, episodes []Episode) {
+//     var ids []string
+//     reader := bytes.NewReader(page)
+//     doc, err := goquery.NewDocumentFromReader(reader)
+//     checkerr(err)
+//     r, err := regexp.Compile(`\/\d+\/`)
+//     checkerr(err)
+//     show.Title = doc.Find(".play_title-page-info__header-title").Text()
+//     show.PlayService = playService
+//     show.PlayId = showId
+//     doc.Find(".play_vertical-list").First().Find("li").Each(func(i int, s *goquery.Selection) {
+//         link, _ := s.Find(".play_vertical-list__header-link").Attr("href")
+//         digi := r.FindString(link)
+//         cleanId := strings.Replace(string(digi), "/", "", 2)
+//         ids = append(ids, cleanId)
+//     })
+//     for _, id := range ids {
+//         e := GetEpisode(id)
+//         episodes = append(episodes, e)
+//     }
+//     return
+// }
 
-/* Fetches episode information for the supplied episode id */
+// GetEpisode parses the information for an episode of a show
+// Returns the episode information
 func GetEpisode(episodeId string) (e Episode) {
-    url := videoUrlBase + episodeId + jsonVideoOutputString
+    url := jsonVideoOutputString + episodeId
     b := getPage(url)
     var p Program
     err := json.Unmarshal(b, &p)
     checkerr(err)
-    e.Broadcasted = time.Now()
-    e.Category = p.Statistics.Category
-    e.Description = ""
-    e.EpisodeNumber = 0
-    e.Length = (time.Duration(p.Video.MaterialLength) * time.Second).String()
-    e.Live = p.Video.Live
-    e.PlayId = p.VideoId
-    e.Season = 0
-    e.Title = p.Context.Title
-    e.Thumbnail = p.Context.ThumbnailImage
-    for _, vref := range p.Video.VideoReferences {
-        if vref.PlayerType == "ios" {
-            e.VideoUrl = vref.Url
-        }
-    }
+    e.Broadcasted = parseDateTime(p.Publish_at)
+    e.Category = p.Format_categories[0].Name
+    e.Description = p.Description
+    e.EpisodeNumber, err = strconv.ParseInt(p.Format_position.Episode, 0, 64)
+    checkerr(err)
+    e.Season = p.Format_position.Season
+    e.Length = (time.Duration(p.Duration) * time.Second).String()
+    // e.Live = p.Video.Live
+    e.PlayId = p.Id
+    e.Title = p.Format_title
+    e.Thumbnail = fixThumbnailAdress(p.Embedded.Format.Links.Image.Href)
+    return
+}
+
+
+// Replaces the size variable in the thumbnail url with the actually wanted size
+func fixThumbnailAdress(url string) string {
+    return strings.Replace(url, "{size}", thumbnailSize, 1)
+}
+
+// parseDateTime parses the date and time for when an episode was broadcasted
+// Returns the date and time as an time object
+func parseDateTime(d string) (datetime time.Time){
+    year := d[0:4]
+    month := d[5:7]
+    day := d[8:10]
+    hour := d[11:13]
+    minute := d[14:16]
+    datetime, _ = time.Parse("2006 01 02 15:04", year + " " + month + " " + day + " " + hour + ":" + minute)
     return
 }
 
@@ -225,8 +238,12 @@ func checkerr(err error) {
 }
 
 func main() {
-    programs := GetAllProgramIds()
-    for _, prog := range programs {
-        fmt.Println(prog)
-    }
+    // Test getting all program ids
+    // programs := GetAllProgramIds()
+    // for _, prog := range programs {
+    //     fmt.Println(prog)
+    // }
+    
+    // Test getting episode info
+    fmt.Println(GetEpisode("556495"))
 }
