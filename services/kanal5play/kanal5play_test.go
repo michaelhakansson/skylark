7375431eb7f29d8ac0e3a5c32dc3c13f22351060
hygerth <hygerth@kth.se>
2015-04-26 13:18:48 +0200

    [kanal5play] Removed trailing whitespace

--- services/kanal5play/kanal5play_test.go
+++ services/kanal5play/kanal5play_test.go
@@ -56,7 +56,7 @@ var episodelinks = []testepisodelinks {
 
 var episodes = []testepisode {
     {"show1episode.json", "", "", "Lars bor med sina tre barn i ett trev\u00e5ningshus p\u00e5 190 kvm i Olofstorp. Lars b\u00f6rjade bygga med sin f\u00f6re detta fru men n\u00e4r paret gick skilda v\u00e4gar avstannade bygget. Nu st\u00e5r Lars ensam med ett gigantiskt skal till hus utan fungerande k\u00f6k eller badrum.",
-     "8", "43m37s", false, 2589533249, "12", "http://lh3.googleusercontent.com/iLDVLeYu3Jx05gcp3TWCAnbGTRKN14T10X6P7AjhKThsTUlnZOpHIr7zEQEoWGqg9VPmvi8_yfqLzHRyvbjZvw", 
+    "8", "43m37s", false, 2589533249, "12", "http://lh3.googleusercontent.com/iLDVLeYu3Jx05gcp3TWCAnbGTRKN14T10X6P7AjhKThsTUlnZOpHIr7zEQEoWGqg9VPmvi8_yfqLzHRyvbjZvw",
     "Familjen bor i ett skal", "http://hls0.00607-od0.dna.qbrick.com/00607-od0/_definst_/smil:20141202/20141202091544808-17bvu2qz6l78v9ud5o6sc6emq-967_ipad/playlist.m3u8"},
     {"show2episode.json", "", "", "Tjejerna forts\u00e4tter till Cypern. H\u00e4r ska de medverka i Cyperns st\u00f6rsta tv-s\u00e5pa. Vad de inte vet \u00e4r att det inneb\u00e4r k\u00e4rleksscener. Dessutom blir det lektioner i att krossa vattenmeloner med huvudet - l\u00e4rare \u00e4r ingen mindre \u00e4n Guinness rekordm\u00e4stare.",
     "7", "43m53s", false, 3398002034, "2", "http://lh3.googleusercontent.com/xCZzz8eHqDgk_evbAA2DR7_0ndOZxDNldia7sbkAny92dTu6ykY4mcEUH1QVQCQjExjtgp46bAJD99kbw9E",
