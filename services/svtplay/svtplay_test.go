f95da7c754c4f923c76e43ce11570654827e5f61
hygerth <hygerth@kth.se>
2015-05-02 19:37:20 +0200

    [svtplay] Tests now runs against snapshots instead of live data

--- services/svtplay/svtplay_test.go
+++ services/svtplay/svtplay_test.go
@@ -1,17 +1,21 @@
 package svtplay
 
 import(
+    "io/ioutil"
+    "log"
     "testing"
 )
 
 
 type testshow struct {
+    testfile string
     id string
     title string
     number int
 }
 
 type testepisode struct {
+    testfile string
     id string
     broadcastedtime string
     category string
@@ -27,40 +31,53 @@ type testepisode struct {
 }
 
 var showsXML = []testshow{
-    {"vetenskapens-varld", "Vetenskapens värld", 17},
-    {"uppdrag-granskning", "Uppdrag granskning", 15},
+    {"vetenskapens-varld.xml", "vetenskapens-varld", "Vetenskapens värld", 18},
+    {"uppdrag-granskning.xml", "uppdrag-granskning", "Uppdrag granskning", 16},
 }
 
 var showsPage = []testshow{
-    {"vetenskapens-varld", "Vetenskapens värld", 15},
-    {"uppdrag-granskning", "Uppdrag granskning", 15},
+    {"vetenskapens-varld.html", "vetenskapens-varld", "Vetenskapens värld", 15},
+    {"uppdrag-granskning.html", "uppdrag-granskning", "Uppdrag granskning", 15},
 }
 
 var episodes = []testepisode{
-    {"2843612", "", "kultur-och-nöje", "Del 4 av 10. Gruppfinal, med bland annat Sisyfos-tävlingen - vätskefyllda pilatesbollar som ska rullas uppför en backe. Några av Sveriges främsta idrottsmän och idrottskvinnor möts i fysiska och psykiska utmaningar för att kora Mästarnas mästare 2015. I Grupp 1 ingår Anette Norberg, Anna Olsson, Magnus Muhrén, Danijela Rundqvist, Glenn Hysén och Björn Lind. Programledare: Micke Leijnegard.",
+    {"mastarnas-mastare-s07e04.json", "2843612", "", "kultur-och-nöje", "Del 4 av 10. Gruppfinal, med bland annat Sisyfos-tävlingen - vätskefyllda pilatesbollar som ska rullas uppför en backe. Några av Sveriges främsta idrottsmän och idrottskvinnor möts i fysiska och psykiska utmaningar för att kora Mästarnas mästare 2015. I Grupp 1 ingår Anette Norberg, Anna Olsson, Magnus Muhrén, Danijela Rundqvist, Glenn Hysén och Björn Lind. Programledare: Micke Leijnegard.",
     "4", "58m37s", false, 2843612, "7",
     "http://www.svt.se/cachable_image/1429226401000/svts/article2849370.svt/ALTERNATES/extralarge/default_title",
     "Avsnitt 4", "http://svtplay18p-f.akamaihd.net/i/se/open/20150417/1360782-004A/EPISOD-1360782-004A-2ae7758f8108a631_,892,144,252,360,540,1584,2700,.mp4.csmil/master.m3u8?cc1=name=Svenska~default=yes~forced=no~uri=http://media.svt.se/download/mcc/wp3/undertexter-wsrt/1360782/1360782-004A/C(sv)/index.m3u8~lang=sv"},
-    {"2867878", "", "nyheter", "Kan ses till sön 3 maj (9 dagar kvar)", "11:00", "1m30s", false, 2867878, "23/4", "http://www.svt.se/cachable_image/1429781701000/svts/article2867877.svt/ALTERNATES/extralarge/default_title",
+    {"svt-nyheter-90-sekunder-23-4-11-00.json", "2867878", "", "nyheter", "Kan ses till imorgon 23.59 (1 dag kvar)", "11:00", "1m30s", false, 2867878, "23/4", "http://www.svt.se/cachable_image/1429781701000/svts/article2867877.svt/ALTERNATES/extralarge/default_title",
     "23/4 11.00", "http://svtplay19i-f.akamaihd.net/i/world/open/20150423/1368669-074A/EPISOD-1368669-074A-208212fd95c96099_,892,144,252,360,540,1584,2700,.mp4.csmil/master.m3u8"},
 }
 
 func TestProgramIds(t *testing.T) {
-    programs := GetAllProgramIds()
-    if len(programs) != 553 {
+    page, err := ioutil.ReadFile("testFiles/allprograms.html")
+    if err != nil {
+        log.Fatal(err)
+    }
+    ids := parseAllProgramsPage(page)
+    if len(ids) != 556 {
         t.Error(
-            "For", "GetAllProgramIds",
-            "expected", 553,
-            "got", len(programs),
+            "For", "parseAllProgramPage, length",
+            "expected", 556,
+            "got", len(ids),
+        )
+    }
+    if ids[5] != "alla-ar-fotografer" {
+        t.Error(
+            "For", "parseAllProgramsPage, id",
+            "expected", "alla-ar-fotografer",
+            "got", ids[5],
         )
     }
 }
 
 func TestXMLParse(t *testing.T) {
     for _, pair := range showsXML {
-        xmlUrl :=  playUrlBase + pair.id + rssUrl
-        b := getPage(xmlUrl)
-        s, e := parseShowXML(b, pair.id)
+        page, err := ioutil.ReadFile("testFiles/" + pair.testfile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        s, e := parseShowXML(page, pair.id)
         if s.Title != pair.title {
             t.Error(
                 "For", pair.id,
@@ -80,9 +97,11 @@ func TestXMLParse(t *testing.T) {
 
 func TestShowPageParse(t *testing.T) {
     for _, pair := range showsPage {
-        pageUrl := playUrlBase + pair.id
-        b := getPage(pageUrl)
-        s, e := parseShowPage(b, pair.id)
+        page, err := ioutil.ReadFile("testFiles/" + pair.testfile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        s, e := parseShowPage(page, pair.id)
         if s.Title != pair.title {
             t.Error(
                 "For", pair.id,
@@ -102,7 +121,11 @@ func TestShowPageParse(t *testing.T) {
 
 func TestGetEpisode(t *testing.T) {
     for _, pair := range episodes {
-        e := GetEpisode(pair.id)
+        page, err := ioutil.ReadFile("testFiles/" + pair.testfile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        e := parseEpisode(page, pair.id)
         if e.Description != pair.description {
             t.Error(
                 "For", pair.id,
