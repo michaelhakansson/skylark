8bd307ff448c09648164f0c77cdf784ac64c3045
hygerth <hygerth@kth.se>
2015-05-04 11:28:05 +0200

    [db] Added some initial functions

--- db/db.go
+++ db/db.go
@@ -43,3 +43,91 @@ func Connect() *mgo.Session {
     return session
 }
 
+func AddShow(title string, playid string, playservice string) bool {
+    session := Connect()
+    defer session.Close()
+    c := session.DB(db).C("shows")
+    count, err := c.Find(bson.M{"playid": playid}).Count()
+    if err != nil || count > 0 {
+        return false
+    }
+    var episodes []Episode
+    err = c.Insert(&Show{Id: bson.NewObjectId(), Title: title, PlayId: playid,
+    PlayService: playservice, Episodes: episodes})
+    if err != nil {
+        log.Fatal(err)
+        return false
+    }
+    log.Printf("Added %s", title)
+    return true
+}
+
+func GetShowByPlayId(playid string) Show {
+    session := Connect()
+    defer session.Close()
+    c := session.DB(db).C("shows")
+    var s Show
+    err := c.Find(bson.M{"playid": playid}).One(&s)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return s
+}
+
+func GetShowById(showid bson.ObjectId) Show {
+    session := Connect()
+    defer session.Close()
+    c := session.DB(db).C("shows")
+    var s Show
+    err := c.Find(bson.M{"_id": showid}).One(&s)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return s
+}
+
+func AddEpisode(showid bson.ObjectId, broadcasted time.Time, category string,
+description string, episodenumber string, length string, live bool, playid int64,
+season string, thumbnail string, title string, videourl string) bool {
+    session := Connect()
+    defer session.Close()
+    c := session.DB(db).C("shows")
+    episode := &Episode{Broadcasted: broadcasted,
+    Category: category, Description: description, EpisodeNumber: episodenumber,
+    Length: length, Live: live, PlayId: playid, Season: season, Thumbnail:
+    thumbnail, Title: title, VideoUrl: videourl}
+    show := GetShowById(showid)
+    for _, e := range show.Episodes {
+        if e.PlayId == playid {
+            UpdateEpisode(showid, *episode)
+            return true
+        }
+    }
+    show.Episodes = append(show.Episodes, *episode)
+    _, err := c.UpsertId(showid, show)
+    if err != nil {
+        log.Fatal(err)
+        return false
+    }
+    log.Printf("Added episode %s to show %s", episode.PlayId, show.Title)
+    return true
+}
+
+func UpdateEpisode(showid bson.ObjectId, episode Episode) bool {
+    session := Connect()
+    defer session.Close()
+    c := session.DB(db).C("shows")
+    show := GetShowById(showid)
+    for _, e := range show.Episodes {
+        if e.PlayId == episode.PlayId {
+            e = episode
+        }
+    }
+    _, err := c.UpsertId(showid, show)
+    if err != nil {
+        log.Fatal(err)
+        return false
+    }
+    log.Printf("Updated episode %s", episode.Title)
+    return true
+}
