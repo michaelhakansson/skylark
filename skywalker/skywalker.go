e0aa4e9149828a8142d34ff1e94a9f70ec3f9147
hygerth <hygerth@kth.se>
2015-05-05 17:12:50 +0200

    [skywalker] Implemented initial version of the syncAll function

--- skywalker/skywalker.go
+++ skywalker/skywalker.go
@@ -1,47 +1,61 @@
 package main
 
 import(
-    // "log"
-    "time"
+    "log"
+    //    "time"
     "github.com/michaelhakansson/skylark/services/svtplay"
     "github.com/michaelhakansson/skylark/services/tv3play"
     "github.com/michaelhakansson/skylark/services/kanal5play"
     "github.com/michaelhakansson/skylark/db"
+    "github.com/michaelhakansson/skylark/structures"
 )
 
 const freshnessLimit float64 = 0.5
 
 func syncAll() {
-    for _, playService := range services {
-        showsIds := playService.GetAllProgramIds()
-
+    var showIds []string
+    var playservice string
+    for _, action := range []func() {
+            func() { showIds, playservice = svtplay.GetAllProgramIds() },
+            func() { showIds, playservice = tv3play.GetAllProgramIds() },
+            func() { showIds, playservice = kanal5play.GetAllProgramIds() }
+        } {
+        action()
         // Add all shows and episodes to the DB
         for _, id := range showIds {
-            syncShow(id)
+            syncShow(id, playservice)
         }
     }
 }
 
-func syncShow(showId string) {
-    show, episodes := playService.GetShow(showId)
+func syncShow(showId string, playservice string) {
+    show, episodes := getShowWithService(showId, playservice)
     _, dbShowObject := db.AddShow(show.Title, show.PlayId, show.PlayService)
 
     for _, episode := range episodes {
-        db.AddEpisode(dbShowObject.Id, episode.Broadcasted, episode.Category,
-            episode.Description, episode.Episodenumber, episode.Length, episode.Live, 
-            episode.Playid, episode.Season, episode.Thumbnail, episode.Title, episode.Videourl)
+        db.AddEpisode(dbShowObject.Id, episode)
     }
 }
 
-func getShowFreshness(show db.Show) (freshness float64) {
+func getShowWithService(showId string, playservice string) (show structures.Show, episodes []structures.Episode) {
+    switch playservice {
+        case "svtplay": show, episodes = svtplay.GetShow(showId)
+        case "tv3play": show, episodes = tv3play.GetShow(showId)
+        case "kanal5play": show, episodes = kanal5play.GetShow(showId)
+    }
+    return
+}
+
+func getShowFreshness(show structures.Show) (freshness float64) {
     for _, episode := range show.Episodes {
         freshness = (freshness + episode.Freshness)
     }
-    freshness /= len(show)
+    freshness /= float64(len(show.Episodes))
     return
 }
 
 func main() {
+    syncAll()
     go func() {
         timer := time.Tick(15 * time.Minute)
         for now := range timer {
